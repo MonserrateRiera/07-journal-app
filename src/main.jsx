132ce226fd4bd71ec639445f46614064c215ed74
Monserrate Riera <mrieramar@uoc.edu>
2024-08-02 18:48:41 +0200

    feat: Update store import path in main.jsx
    The import path for the store module in main.jsx is updated to './store/' to match the new directory structure. This change ensures that the correct store is imported and used in the application.
    
    This commit improves the organization and maintainability of the code by updating the import path to reflect the new location of the store module.

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './styles.css'
 import JournalApp from './JournalApp.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './store'
+import { store } from './store/'
 
 
 
